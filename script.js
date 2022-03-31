c5da7686c8da6fe776a30c6bab814b0b68b82be9
Roiney Beal <nei_beal@hotmail.com>
2022-03-31 17:39:44 -0300

    final requisito 3

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const inputSenha = document.getElementById('senha');
 const btnEntra = document.getElementById('btn');
 
 function entrar() {
-  if (inputNome.value === 'tryber@RuleTester.com' && inputSenha.value === '123456') {
+  if (inputNome.value === 'tryber@teste.com' && inputSenha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
