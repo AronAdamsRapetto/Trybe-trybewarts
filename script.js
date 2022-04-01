a1765b597d15a591a80464cf6875ca8588a6f029
Aron Adams <aronrapetto@hotmail.com>
2022-04-01 00:39:50 -0300

    problema do requisito 5 resolvido, requisito 20 concluido

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputNome = document.getElementById('nome');
 const inputSenha = document.getElementById('senha');
 const btnEntra = document.getElementById('btn');
+const blocoTexto = document.getElementById('textarea');
 
 function entrar() {
   if (inputNome.value === 'tryber@teste.com' && inputSenha.value === '123456') {
@@ -10,3 +11,13 @@ function entrar() {
   }
 }
 btnEntra.addEventListener('click', entrar);
+
+function contaCaracteres() {
+  const tamanhoTexto = document.getElementById('textarea').value;
+  const textoContador = document.getElementById('counter');
+  let tamanhoRestante = 500;
+  tamanhoRestante -= tamanhoTexto.length;
+  textoContador.innerText = `${tamanhoRestante} caracteres restantes`;
+}
+
+blocoTexto.addEventListener('keyup', contaCaracteres);
